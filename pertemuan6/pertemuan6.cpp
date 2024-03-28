37c96447ee5d2270a9c6336e11fed320e9878015
firyalshafa <shafafiryal782@gmail.com>
2024-03-28 16:14:06 +0800

    memperbaiki display

--- pertemuan6/pertemuan6.cpp
+++ pertemuan6/pertemuan6.cpp
@@ -88,7 +88,7 @@ void display() {
     cout << "sorted array" << endl;
     cout << "------------------" << endl;
 
-    for (int i + 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
