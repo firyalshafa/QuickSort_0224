610ac2bde46f07caf48cc760cdb9db7b88280a26
firyalshafa <shafafiryal782@gmail.com>
2024-03-28 15:11:32 +0800

    Add project files.

---
+++ pertemuan6/pertemuan6.cpp
@@ -0,0 +1,13 @@
+// pertemuan6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+
