f7cc97e12153f7b6860cd542d36f49bbcde6fcca
firyalshafa <shafafiryal782@gmail.com>
2024-03-28 15:52:26 +0800

    membuat display

--- pertemuan6/pertemuan6.cpp
+++ pertemuan6/pertemuan6.cpp
@@ -83,6 +83,19 @@ void q_short(int low, int high)
 
 }
 
+void display() {
+    cout << "/n---------------" << endl;
+    cout << "sorted array" << endl;
+    cout << "------------------" << endl;
+
+    for (int i + 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "/n/nnumber  of comparasions:" << cmp_count << endl;
+    cout << "number of data movements:" << mov_count << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
