f175b352e5763e3b370a071df9cb45724f4f56c1
firyalshafa <shafafiryal782@gmail.com>
2024-03-28 15:38:35 +0800

    membuat step 8

--- pertemuan6/pertemuan6.cpp
+++ pertemuan6/pertemuan6.cpp
@@ -62,7 +62,11 @@ void q_short(int low, int high)
             cmp_count++;
         }
         cmp_count++;
-        while ( (arr[j] > pivot) && (j >= low))  // step 7
+        while ((arr[j] > pivot) && (j >= low))  // step 7
+        {
+            j--; // step 8
+            cmp_count++;
+        }
     }
 }
 
