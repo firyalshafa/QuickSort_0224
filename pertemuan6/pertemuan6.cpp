988035343bbb1923aa9c0fdd2e6b6196e6c74943
firyalshafa <shafafiryal782@gmail.com>
2024-03-28 16:10:20 +0800

    membuat output

--- pertemuan6/pertemuan6.cpp
+++ pertemuan6/pertemuan6.cpp
@@ -98,7 +98,20 @@ void display() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char ch;
+    
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\ndo you  want to continue? (y/n):";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+        system("pause");
+        system("cls");
+    }
+
 }
 
 
